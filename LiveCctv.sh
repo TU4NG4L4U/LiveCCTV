f0716e7206015a9c22338121fdcfbbee52d011aa
TU4NG4L4U <46773642+TU4NG4L4U@users.noreply.github.com>
2019-03-24 08:04:23 +0700

    Add files via upload

---
+++ LiveCctv.sh
@@ -0,0 +1,103 @@
+clear
+#usr/bin/bash
+#Jangan Recode Hargailah Pembuatnya
+#By TU4N G4L4U
+
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+sleep 1
+
+echo "\033[31;1m      _____"
+echo "\033[31;1m_[]__/     \__n_"
+echo "\033[31;1m|______,--,__()_|"
+echo "\033[31;1m|     /    \    |"
+echo "\033[31;1m|     \    /    |"
+echo "\033[31;1m|      •--•     |"
+echo "\033[31;1m|_______________|"
+echo
+echo "\033[32;1m,------------------------------""\033[37;1m------------------------------,"
+echo "\033[32;1m|"  "\033[31;1m[]Athuor : TU4N G4L4U"                                   "|"
+echo "\033[32;1m|"  "\033[31;1m[]Pembuatan : 23 Maret 2019"                             "|"
+echo "\033[32;1m|"  "\033[31;1m[]My Team : INDONESIAN CYBER TEAM & JAKUT CYBER ARMY"    "|"
+echo "\033[32;1m|"  "\033[31;1m[]My Friedns : Sk4ters & MR.1Z4K412"                     "|"
+echo "\033[32;1m,------------------------------""\033[37;1m------------------------------,"
+echo
+echo
+echo $cyan ">>"  $blue "1.INDONESIA"  $cyan "<<"
+echo $cyan ">>"  $blue "  2.JAPAN  "  $cyan "<<"
+echo $cyan ">>"  $blue "  3.JERMAN "  $cyan "<<"
+echo
+read -p "[]Pilih>> " pilih
+if [ $pilih = 1 ]
+then
+clear
+figlet -f slant W A I T | lolcat
+echo
+echo
+echo $red "Not"
+sleep 1
+echo $red "Not"
+sleep 1
+echo $red "Not"
+sleep 1
+echo $red "Not"
+echo
+echo $purple ">>" $white "http://116.68.252.222:83" $purple "<<"
+echo $purple ">>" $white "http://202.74.79.122:8000" $purple "<<"
+echo $purple ">>" $white "http://202.58.207.176:8090" $purple "<<"
+echo $purple ">>" $white "http://116.68.252.222:82" $purple "<<"
+echo $purple ">>" $white "http://210.23.68.3:88" $purple "<<"
+echo
+echo $red "Terima kasih Sudah Memakai Tools Saya"
+echo
+echo
+fi
+
+if [ $pilih = 2 ]
+then
+clear
+figlet -f slant W A I T | lolcat
+echo
+echo
+echo $red "Not"
+sleep 1
+echo $red "Not"
+sleep 1
+echo $red "Not"
+sleep 1
+echo $red "Not"
+echo
+echo $purple ">>" $white "http://219.117.201.193:2002" $purple "<<"
+echo $purple ">>" $white "http://114.153.246.172:82" $purple "<<"
+echo $purple ">>" $white "http://114.162.104.168:60001" $purple "<<"
+echo $purple ">>" $white "http://110.5.60.250:8084" $purple "<<"
+echo $purple ">>" $white "http://202.229.246.245:60001" $purple "<<"
+echo
+echo $red "Terima Kasih Sudah Memakai Tools Saya"
+echo
+echo
+fi
+
+if [ $pilih = 3 ]
+then
+clear
+figlet -f slant W A I T | lolcat
+echo
+echo
+echo "[ http://195.243.58.195:80 ]"
+echo "[ http://80.153.171.94:80 ]"
+echo "[ http://217.92.223.122:8080 ]"
+echo "[ http://80.153.58.187:8081 ]"
+echo "[ http://80.149.163.196:80 ]"
+echo
+echo $red "Terima kasih Sudah Memakai Tools Saya"
+echo
+echo
+fi
